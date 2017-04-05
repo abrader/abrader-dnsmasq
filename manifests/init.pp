86c792de039447117d5d30630d35fdbec1b7c3a8
Andrew Brader <abrader@gmail.com>
2017-04-05 08:36:31 -0400

    First code commit
    Added the template for the DNSMASQ config and early days code to test
    EPP template transformation.

---
+++ manifests/init.pp
@@ -0,0 +1,21 @@
+# DNSMasq Puppet Module
+class dnsmasq (
+  Array[String] $interfaces         = ['p6p1'],
+  Array[String] $no_dhcp_interfaces = ['lo', 'ham0', 'p6p1', 'p6p2']
+){
+
+  pacakge { 'dnsmasq' :
+    ensure => present,
+  }
+
+  file { 'dnsmasq_config_file' :
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    path    => '/tmp/dnsmasq.conf',
+    content => epp('dnsmasq.conf.epp'),
+    require => Package['dnsmasq'],
+  }
+
+}
